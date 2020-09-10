bd1febd28f0434e3cc9b5dc75b7047d40b442748
Resedit-git <acis.lan@gmail.com>
2020-09-10 10:19:04 +0300

    Now it work

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 import Routes from './Routes.js'
 
+
 class App extends Component {
+
   render() {
     return (
         <Routes />
--- routes/game.js
+++ routes/game.js
@@ -1,59 +1,59 @@
 import React, {useState} from 'react';
-import {Text, View, StyleSheet, Image, Button, TouchableOpacity, Alert, Animated} from 'react-native';
-import {Actions} from 'react-native-router-flux';
+import {Text, View, StyleSheet} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
 import * as Levels from '../levels';
 
-function shuffleArray(array) {
-    let i = array.length - 1;
-    for (i; i > 0; i--) {
-        const index = Math.floor(Math.random() * (i + 1));
-        const a = array[index];
-        array[index] = array[i];
-        array[i] = a;
-    }
-
-    return array;
-}
 
 const Game = (props) => {
-    const goToHome = () => {
-        Actions.home()
-    };
 
     const levelID = props.level;
     const complexity = props.complexity;
     const level = Levels[complexity][levelID];
-    let letters = shuffleArray(level.letters);
-    let [disabledLetter, setDisabledLetter] = useState( []);
+    let letters = level.letters;
+    let [disabledLetter, setDisabledLetter] = useState([]);
     let [answer, setAnswer] = useState([]);
 
-    function validate(e) {
-        answer = setAnswer(`${answer}${e}`);
+    function createLetters(letter, index) {
+        if (disabledLetter.includes(index)) {
+            return (
+                <Text style={[styles.letters, styles.textCenter, styles.lettersText, {flexWrap: 'nowrap'}]}
+                      disabled
+                >
+                    {letter}
+                </Text>
+            );
+        } else {
+            return (
+                <Text style={[styles.letters, styles.textCenter, styles.lettersText, {flexWrap: 'nowrap'}]}
+                      onPress={() => validate(letter, index)}
+                >
+                    {letter}
+                </Text>
+            );
+        }
+    }
 
+    function validate(e, index) {
+        answer = setAnswer(`${answer}${e}`);
 
         //const index = letters.indexOf(e);
         //letters.splice(index, 1);
-        disabledLetter = setDisabledLetter(letters)
+
+        disabledLetter = setDisabledLetter(`${disabledLetter}, ${index}`)
     }
 
     const rows = letters.map((letter, index) =>
 
         <View key={index}>
-            <Text style={[styles.letters, styles.textCenter, styles.lettersText, {flexWrap: 'nowrap'}]}
-                  onPress={() => validate(letter)}
-                  disabled={disabledLetter.includes(letter)}
-            >
-                {letter}
-            </Text>
-        </View>
+            {createLetters(letter, index)}
+        </View>,
     );
 
     return (
         <LinearGradient
-            start={{x: 0, y: 1}} end={{x: 0, y: 0}}
-            colors={["#2c5aff", "#0076ff", "#008cff", "#009fff", "#43a1ff", "#5fa2ff", "#74a4ff", "#a494f6", "#cc82e1"]}
+            start={{x: 0, y: 1}} end={{x: 1, y: 0}}
+            colors={['#2c5aff', '#0076ff', '#008cff', '#009fff', '#43a1ff', '#5fa2ff', '#74a4ff', '#a494f6', '#cc82e1']}
             style={{flex: 1}}
         >
 
@@ -96,7 +96,7 @@ const styles = StyleSheet.create({
     },
     lettersText: {
         fontFamily: 'Cochin',
-        color: 'black',
+        color: 'white',
         fontSize: 25,
         fontWeight: 'bold',
     },
@@ -109,11 +109,22 @@ const styles = StyleSheet.create({
         marginTop: 50,
         justifyContent: 'space-evenly',
         padding: 10,
-        borderRadius: 25,
-        borderColor: 'white',
-        borderWidth: 1,
+        paddingBottom: 30,
         flexDirection: 'row',
         alignItems: 'center',
+        borderColor: 'white',
+        borderBottomWidth: 1,
+
+    },
+    letters: {
+        borderRadius: 50,
+        alignItems: 'center',
+        width: 40,
+        height: 40,
+        margin: 3,
+        marginTop: 10,
+        borderColor: 'white',
+        borderWidth: 1,
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -121,21 +132,15 @@ const styles = StyleSheet.create({
         },
         shadowOpacity: 0.25,
         shadowRadius: 16.84,
-        elevation: 3,
-    },
-    letters: {
-        borderRadius: 50,
-        alignItems: 'center',
-        width: 40,
-        height: 40,
-        backgroundColor: 'white',
-        margin: 3,
-        marginTop: 10,
+        elevation: 2,
+
+        backgroundColor: 'rgba(95,162,255, 0.5)',
+
     },
     lettersContainer: {
         width: 280,
         height: 150,
-        marginTop: 50,
+        marginTop: 60,
     },
     lettersSpace: {
         flex: 1,
@@ -145,22 +150,12 @@ const styles = StyleSheet.create({
         justifyContent: 'space-evenly',
     },
     answerContainer: {
-        borderRadius: 25,
         alignItems: "center",
         borderColor: 'white',
-        borderWidth: 1,
+        borderBottomWidth: 1,
         width: 200,
-        height: 60,
-        marginTop: 80,
-        justifyContent: 'space-evenly',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 16.84,
-        elevation: 3,
+        marginTop: 60,
+        paddingBottom: 10,
     },
     answerText: {
         fontFamily: 'Cochin',
@@ -183,11 +178,12 @@ const styles = StyleSheet.create({
         paddingRight: 15,
         borderRadius: 5,
     },
-    buttonContainer:{
+    buttonContainer: {
         marginBottom: 10,
         paddingLeft: 40,
         paddingRight: 40,
         borderRadius: 25,
     },
 });
+
 export default Game;
